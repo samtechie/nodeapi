a268e1df4469599e9ad8cf6756acf36017635b56
Wasswa Samuel <latimerscope@gmail.com>
2020-10-20 22:05:04 +0200

    Add dynamic responses

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const querystring = require('querystring')
 
 const port = process.env.PORT || 1337
 
@@ -16,9 +17,31 @@ function respondNotFound(req, res) {
   res.end('Not Found')
 }
 
+function respondEcho(req, res) {
+  const { input = '' } = querystring.parse(
+    req.url
+      .split('?')
+      .slice(1)
+      .join('')
+  )
+  res.setHeader('Content-Type', 'application/json')
+  res.end(
+    JSON.stringify({
+      normal: input,
+      shouty: input.toUpperCase(),
+      characterCount: input.length,
+      backwards: input
+        .split('')
+        .reverse()
+        .join('')
+    })
+  )
+}
+
 const server = http.createServer(function (req, res) {
   if (req.url === '/') return respondText(req, res)
   if (req.url === '/json') return respondJson(req, res)
+  if (req.url.match(/^\/echo/)) return respondEcho(req, res)
   respondNotFound(req, res)
 })
 
